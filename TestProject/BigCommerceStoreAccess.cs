e5e5db8caa6c4652a2189083465a29be5cc98ece
José Alejandro Lugo-García <jalugogarcia@gmail.com>
2023-11-20 17:31:35 -0500

    Add TestProject.

---
+++ TestProject/BigCommerceStoreAccess.cs
@@ -0,0 +1,69 @@
+﻿using BigCommerceNET.Models.Configuration;
+using BigCommerceNET;
+using BigCommerceNET.Models.Category;
+using BigCommerceNET.Models.Product;
+
+namespace TestProject
+{
+    public class BigCommerceStoreAccess
+    {
+        private readonly IBigCommerceFactory BigCommerceFactory = new BigCommerceFactory();
+        private BigCommerceConfig ConfigV3;
+
+        private const string ShortShopName = "your-shop-name";
+        private const string ClientId = "your-client-id";
+        private const string ClientSecret = "your-client-secret";
+        private const string AccessToken = "your-access-token";
+
+        public BigCommerceStoreAccess()
+        {
+            this.ConfigV3 = new BigCommerceConfig(ShortShopName, ClientId, ClientSecret, AccessToken);
+        }
+
+        /// <summary>
+        /// Gets the list of Categories from the BigCommerce store.
+        /// </summary>
+        public List<BigCommerceCategory> GetCategories()
+        {
+            try
+            {
+                var service = this.BigCommerceFactory.CreateCategoriesService(this.ConfigV3);
+
+                List<BigCommerceCategory> categories = service.GetCategories();
+
+                return categories;
+            }
+            catch (System.Net.WebException webEx)
+            {
+                if (webEx.Message.Contains("Cloudflare"))
+                    throw new Exception("Could not access the web server. Check Cloudflare IP whitelist rules.");
+                else
+                    throw new Exception("Could not access the web server. Check Firewall rules.");
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Gets the list of Products from the BigCommerce store.
+        /// </summary>
+        public async Task<List<BigCommerceProduct>> GetProducts()
+        {
+            try
+            {
+                var service = this.BigCommerceFactory.CreateProductsService(this.ConfigV3);
+
+                var products = await service.GetProductsAsync(CancellationToken.None, true);
+
+                return products;
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
---
+++ TestProject/Program.cs
@@ -0,0 +1,19 @@
+﻿using BigCommerceNET.Models.Category;
+using BigCommerceNET.Models.Product;
+using TestProject;
+
+BigCommerceStoreAccess BCAccess = new BigCommerceStoreAccess();
+List<BigCommerceCategory> categories = BCAccess.GetCategories();
+List<BigCommerceProduct> products = await BCAccess.GetProducts();
+
+foreach (var category in categories)
+{
+    Console.WriteLine($"Category: {category.Category_Name}");
+}
+
+foreach (var product in products)
+{
+    Console.WriteLine($"Product: {product.Name}");
+}
+
+Console.ReadLine();
