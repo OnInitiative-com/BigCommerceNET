68591799da458ef9555b4e2e82b972cfe9e51312
José Alejandro Lugo-García <jalugogarcia@gmail.com>
2023-11-27 09:37:08 -0500

    Improve console app.

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -6,10 +6,14 @@ BigCommerceStoreAccess BCAccess = new BigCommerceStoreAccess();
 List<BigCommerceCategory> categories = BCAccess.GetCategories();
 List<BigCommerceProduct> products = await BCAccess.GetProducts();
 
+Console.WriteLine("Categories:");
 foreach (var category in categories)
 {
     Console.WriteLine($"Category: {category.Category_Name}");
 }
+
+Console.WriteLine();
+Console.WriteLine("Products:");
 
 foreach (var product in products)
 {    
